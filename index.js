34b5f41a5623a337101188c2de5ce12ca91159ee
iwonaive <szymaniak.iwona11@gmail.com>
2024-03-26 21:36:10 +0100

    added menu

---
+++ index.js
@@ -0,0 +1,16 @@
+
+const navToggle = document.querySelector(".nav-toggle");
+const mainMenu = document.querySelector(".main-menu");
+const navToggleLabel = document.querySelector(".nav-toggle-label");
+
+navToggle.addEventListener("click", () => {
+  mainMenu.classList.toggle("active");
+  navToggleLabel.classList.toggle("active");
+});
+
+mainMenu.addEventListener("click", (event) => {
+  if (event.target) {
+    mainMenu.classList.remove("active");
+    navToggleLabel.classList.remove("active");
+  }
+});
